181ab9ad0793e418f9ddfe7598b5e0e1e7b063dd
ahwz <hanweiz@gmail.com>
2022-06-03 22:32:35 +0800

    Snake movingsmoothly with no user inputs.

--- main.py
+++ main.py
@@ -1,22 +1,40 @@
 from turtle import Turtle, Screen 
 import random
+import time
+import turtle
+
+# define constants
+START_SEGMENTS = 3
+turtle.speed("fast")
 
 # create the screen background
 screen = Screen()
 screen.setup(height=600, width=600)
 screen.bgcolor("black")
 screen.title("Snaky Eats its Tail")
+screen.tracer(0)
 
 # create snake body
-seg1 = Turtle("square")
-seg1.color("white")
-seg1.home()
+segments = []
+for index in range(START_SEGMENTS):
+    seg = Turtle("square")
+    seg.color("white")
+    seg.penup()
+    seg.goto((index * -20), 0)
+    segments.append(seg)
 
-seg2 = Turtle("square")
-seg2.color("white")
-seg2.goto(-20, 0)
+is_game_on = True
+while is_game_on:
+    screen.update()
+    time.sleep(0.1)
 
-seg3 = Turtle("square")
-seg3.color("white")
-seg3.goto(-40, 0)
+    for seg_idx in range(len(segments) - 1, 0, -1):
+        # get all segments to follow the head
+        x_new = segments[seg_idx - 1].xcor()
+        y_new = segments[seg_idx - 1].ycor()
+        segments[seg_idx].goto(x_new, y_new)
+    # then move the head
+    segments[0].forward(20)
+    segments[0].left(90)
 screen.exitonclick()
+
